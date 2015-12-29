beb1ada18f0e50d39620aed06aac3903be1e858a
Elijah Kim <kimelijah90@gmail.com>
2015-12-29 15:13:14 -0500

    Create env variable

--- lib/issues/github_issues.ex
+++ lib/issues/github_issues.ex
@@ -1,5 +1,6 @@
 defmodule Issues.GithubIssues do
   @user_agent [ { "User-agent", "Eli eli@elijah.kim" } ]
+  @github_url Application.get_env(:issues, :github_url)
 
   def fetch(user, project) do
     issues_url(user, project)
@@ -8,9 +9,9 @@ defmodule Issues.GithubIssues do
   end
 
   def issues_url(user, project) do
-    "https://api.github.com/repos/#{user}/#{project}/issues"
+    "#{@github_url}/repos/#{user}/#{project}/issues"
   end
 
-  def handle_response({ :ok, body }), do: { :ok, body }
-  def handle_response({ ___, body }), do: { :error, body }
+  def handle_response({ :ok, body }), do: { :ok, :jsx.decode(body.body) }
+  def handle_response({ ___, body }), do: { :error, :jsx.decode(body.body) }
 end
